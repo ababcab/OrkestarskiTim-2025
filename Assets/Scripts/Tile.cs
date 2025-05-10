4a923ca0aa46172c7945ec476c9799ec0811a3f3
Froggo <nikolakuzmanovic45@gmail.com>
2025-05-10 18:19:55 +0200

    Ubačeni žurka i kiflice prefabovi
    Što naslov kaže

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,65 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public bool zauzeto = false;
+    public GameObject dropdown;
+    public GameObject sator_prefab;
+    public GameObject kiflice_prefab;
+    public GameObject parent_grid;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        //highlight
+        if (dropdown.GetComponent<GetValueFromDropdown>().selectedOption == "Sator")
+        {
+            this.gameObject.GetComponent<MeshRenderer>().enabled = true;
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0) && zauzeto == false)
+        {
+            if (dropdown.GetComponent<GetValueFromDropdown>().selectedOption == "Sator")
+            {
+                //placeSator();
+                //lose money
+                //+cacije
+                Debug.Log("place sator");
+                zauzeto = true;
+
+                Instantiate(sator_prefab, this.transform.position, Quaternion.identity);
+            } 
+            else if (dropdown.GetComponent<GetValueFromDropdown>().selectedOption == "Kiflice")
+            {
+                Debug.Log("place kiflice");
+                zauzeto = true;
+
+                Instantiate(kiflice_prefab, this.transform.position, Quaternion.identity);
+            }
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (dropdown.GetComponent<GetValueFromDropdown>().selectedOption == "Sator")
+        {
+            this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+}
