ef6c00a01301235183eab940e43d552147c07cdb
Andrew Striker <andric692@gmail.com>
2025-05-10 19:55:43 +0200

    Update Grid.cs

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -23,9 +23,9 @@ public class GridSystem : MonoBehaviour
         float offset = this.transform.localScale.x * offsetAmount;
         Debug.Log(this.transform.localScale.x);
 
-        for (int x = 0; x < width; x++)
+        for (int x = -width/2; x < width/2; x++)
         {
-            for(int y = 0; y < height; y++)
+            for(int y = -height/2; y < height/2; y++)
             {
                 var spawnedTile = Instantiate(tilePrefab, new Vector3(x * offset, 0, y * offset), Quaternion.identity, gameObject.transform);
                 spawnedTile.name = $"Tile {x} {y}";
