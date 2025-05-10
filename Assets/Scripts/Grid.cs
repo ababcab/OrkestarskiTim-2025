6fd7cd667f6f528e728f9a2d72b6acab2f287b00
Andrew Striker <andric692@gmail.com>
2025-05-10 19:51:28 +0200

    grid changes v2

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -6,6 +6,7 @@ public class GridSystem : MonoBehaviour
     public int width;
     public int height;
     public GameObject tilePrefab;
+    public int offsetAmount;
 
     private void Awake()
     {
@@ -19,12 +20,12 @@ public class GridSystem : MonoBehaviour
 
     void GenerateGrid()
     {
-        var offset = this.transform.localScale.x * 10;
+        float offset = this.transform.localScale.x * offsetAmount;
         Debug.Log(this.transform.localScale.x);
 
-        for (int x = -width/2; x < width/2; x++)
+        for (int x = 0; x < width; x++)
         {
-            for(int y = -height/2; y < height/2; y++)
+            for(int y = 0; y < height; y++)
             {
                 var spawnedTile = Instantiate(tilePrefab, new Vector3(x * offset, 0, y * offset), Quaternion.identity, gameObject.transform);
                 spawnedTile.name = $"Tile {x} {y}";
