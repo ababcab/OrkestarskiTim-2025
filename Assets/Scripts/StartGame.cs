215ce8fb6dd5f88db98145d185889d483f6ff3e4
Andrew Striker <andric692@gmail.com>
2025-05-13 14:36:56 +0200

    main menu + ui improvements

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+    [SerializeField]
+    private Button startButton;
+    [SerializeField]
+    private AudioSource soundSource;
+    [SerializeField]
+    private AudioClip ambience;
+
+    public float masterSoundVolume = 1;
+    [Range(0, 1f)]
+    public float volModifier1 = 1f;
+
+
+    private void Start()
+    {
+        startButton.onClick.AddListener(TaskOnClicked);
+        PlaySound(volModifier1, ambience);
+    }
+
+    void TaskOnClicked()
+    {
+        SceneManager.LoadScene("Scenes/LevelLoader");
+    }
+
+    private void PlaySound(float volModifier, AudioClip myClip)
+    {
+        soundSource.clip = myClip;
+        soundSource.volume = masterSoundVolume * volModifier;
+        soundSource.Play();
+    }
+}
