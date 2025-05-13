92e0c4b74ab11c3d407f11249fcdbff9501d0668
PenProgrammer <disciplinaa665@gmail.com>
2025-05-13 12:06:15 +0200

    Dolley zoom added; Usta nestaju tho

--- Assets/Scripts/CameraZoom.cs
+++ Assets/Scripts/CameraZoom.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class CameraZoom : MonoBehaviour
 {
+    [SerializeField]
+    [Range(0,1f)]
+    private float dolleyZoomCoef = 1f;
+
     float minFov = 45f;
-    float maxFov = 65f;
+    float maxFov = 63f;
     float sens = 10f;
+
+    private Transform cam;
+    private Vector3 defaultPosition;
+    private void Start()
+    {
+        cam = Camera.main.transform;
+        defaultPosition = cam.position;
+    }
 
     private void Update()
     {
         float fov = Camera.main.fieldOfView;
         fov -= Input.GetAxis("Mouse ScrollWheel") * sens;
         fov = Mathf.Clamp(fov, minFov, maxFov);
+
+        float fovDifference = maxFov - fov;
+        Vector3 forwardOffset = (-1) * cam.transform.forward * fovDifference;
+
         Camera.main.fieldOfView = fov;
+        Camera.main.transform.position = defaultPosition + forwardOffset * dolleyZoomCoef;
     }
 }
