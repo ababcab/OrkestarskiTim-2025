7d898b550159ec3376f3040a7f154e808a74501a
PenProgrammer <disciplinaa665@gmail.com>
2025-05-14 13:59:22 +0200

    Ui properly scales; Cutscene properly plays on PC

--- Assets/Cutscene/Begin Loading after delay.cs
+++ Assets/Cutscene/Begin Loading after delay.cs
@@ -1,15 +1,33 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Video;
 
 public class BeginLoadingafterdelay : MonoBehaviour
 {
+    public VideoPlayer videoPlayer;
     public VideoClip video;
 
     // Update is called once per frame
+
+
+    private bool videoEnded = false, videoStarted = false;
+    private float videoEndedAt = -1f;
     void Update()
-    {
-        if (Time.time >= video.length + 2f)
+    { 
+        if(videoPlayer.isPlaying)
+        {
+            videoStarted = true;
+        }
+        else if( videoStarted == true && videoEnded == false)
+        {
+            videoEndedAt = Time.time;
+            videoEnded = true;
+        }
+
+        if (videoEnded && Time.time >= videoEndedAt + 2f) 
             SceneManager.LoadScene("Scenes/LevelLoader");
     }
+
+
 }
